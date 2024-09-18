1a3d9e16876f78a9dd0862d89ccdb86a6a4dc17a
Hanna Burkovskaya <56536953+BurkovskayaAD@users.noreply.github.com>
2024-09-18 20:41:12 +0300

    add addTodoRedux

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
 import Card from './components/Card/Card';
 import Header from './components/Header/Header';
 import { useEffect, useState } from 'react';
+import { addTodoRedux } from './slice/todo';
 
 interface ITodo {
   id: number,
@@ -13,14 +15,9 @@ interface ITodo {
 function App() {
 
   const [inputText, setInputText] = useState("");
-  const [todos, setTodos] = useState(() => {
-    let todoFromLS = localStorage.getItem("todos");
-    return todoFromLS ? JSON.parse(todoFromLS) : [];
-  });
 
-  useEffect(() => {
-    localStorage.setItem("todos", JSON.stringify(todos))
-  }, [todos])
+  const dispatch = useDispatch();
+  const todos = useSelector((state: any) => state.todo)
 
   function addTodo() {
       if (inputText !== "") {
@@ -32,38 +29,38 @@ function App() {
               isChecked: false
           }
           // состояние не должно изменяться напрямую
-          setTodos([...todos, todoObject]);
+          dispatch(addTodoRedux(todoObject))
       }
       setInputText("");
   }
 
   function deleteAllTodo() {
-      setTodos([]);
+      // setTodos([]);
   }
 
   function remove(id: number) {
-    const newTodos = todos.filter((todo:ITodo) => {
-      return todo.id !== id;
-    });
-    setTodos(newTodos);
+    // const newTodos = todos.filter((todo:ITodo) => {
+    //   return todo.id !== id;
+    // });
+    // setTodos(newTodos);
   }
 
   function changeTodo(id: number) {
-    const newTodos = [...todos];
-    const current = newTodos.find(item => item.id === id);
-    if (current != undefined) {
-      current.isChecked = !current.isChecked;
-    }
-    setTodos(newTodos);
+    // const newTodos = [...todos];
+    // const current = newTodos.find(item => item.id === id);
+    // if (current != undefined) {
+    //   current.isChecked = !current.isChecked;
+    // }
+    // setTodos(newTodos);
   }
 
   return (
     <>
       <div className='container'>
         <Header inputText={inputText} setInputText={setInputText} addTodo={addTodo} deleteAllTodo={deleteAllTodo}></Header>
-          {todos.length > 0 ? (
+          {todos.todo.length > 0 ? (
             <div className='card-container'>
-            {todos.map((item: ITodo, index: number) => <Card key={index} oneTodo={item} remove={remove} changeTodo={changeTodo}></Card>)}
+            {todos.todo.map((item: ITodo, index: number) => <Card key={index} oneTodo={item} remove={remove} changeTodo={changeTodo}></Card>)}
           </div>
           ): null}
       </div>
--- src/slice/todo.ts
+++ src/slice/todo.ts
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, current } from "@reduxjs/toolkit";
 
 const todoSlice = createSlice({
     name: "todo",
@@ -6,10 +6,37 @@ const todoSlice = createSlice({
         todo: []
     },
     reducers: {
-
+        addTodoRedux(state: any, {payload}: {payload: any}) {
+            // console.log(current(state))
+            // console.log(payload)
+            state.todo.push(payload);
+            // console.log(current(state))
+        }
     }
 })
+
 
 const {actions, reducer} = todoSlice;
 
 export default reducer;
+export const {addTodoRedux} = actions;
+
+
+
+
+
+
+// function addTodo() {
+//     if (inputText !== "") {
+//         let formatDate = String(new Date()).slice(4, 10);
+//         let todoObject = {
+//             id: Date.now(),
+//             text: inputText,
+//             date: formatDate,
+//             isChecked: false
+//         }
+//         // состояние не должно изменяться напрямую
+//         setTodos([...todos, todoObject]);
+//     }
+//     setInputText("");
+// }
